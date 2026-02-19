6ed0fb4c1fc5bb038d0c287b7076faf4f12c4988
Iqram Zargar <shah.iqu159@gmail.com>
2026-02-19 18:02:24 +0530

    app.py modified

--- Advance ML/sentiment_analysis_movie_review/app/streamlit_app.py
+++ Advance ML/sentiment_analysis_movie_review/app/streamlit_app.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import streamlit as st
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))  # safer than abspath
 MODEL_PATH = os.path.normpath(os.path.join(BASE_DIR, '..', 'model', 'model.pkl'))
